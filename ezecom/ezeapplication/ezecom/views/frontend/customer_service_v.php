55b00d6ea09e737619274afa899af60347b460e2
chan.raksmey <chan.raksmey@ezecomcorp.com>
2018-05-31 15:01:51 +0700

    change image to be the same size community
    change image to be the same size community

--- ezecom/ezeapplication/ezecom/views/frontend/customer_service_v.php
+++ ezecom/ezeapplication/ezecom/views/frontend/customer_service_v.php
@@ -17,7 +17,9 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12 pd-right clear-pd-left">
-				<img  class="img-responsive max-size" src="<?php echo base_url()?>images/support/customer-service/customer-service.jpg"/>
+				<div class="customer-service-img">
+					<img  class="img-responsive max-size" src="<?php echo base_url()?>images/support/customer-service/customer-service.jpg"/>
+				</div>
 			</div>
 			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12 pd-left clear-pd-right">
 				<div class="customer-service">
