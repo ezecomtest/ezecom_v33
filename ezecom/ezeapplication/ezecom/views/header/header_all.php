6db19a0831446c0b17b11c4e0e29c183abe5d918
chan.raksmey <chan.raksmey@ezecomcorp.com>
2018-06-12 09:46:05 +0700

    done menu fixed
    done menu fixed

--- ezecom/ezeapplication/ezecom/views/header/header_all.php
+++ ezecom/ezeapplication/ezecom/views/header/header_all.php
@@ -21,11 +21,11 @@
 	</div>
 </div>
 
-<div id="top-header-fix">
+<!--<div id="top-header-fix">
 	<div class="container middle-block">
 		<div class="row" style="padding:15px 0">
 			<div class="col-xl-6 col-lg-6 col-md-6 col-xs-6" style="padding-left:0">
-				<a href="<?php echo base_url(); ?>" title="EZECOM Cambodia Premuim Internet Service Provider">
+				<a href="<?php //echo base_url(); ?>" title="EZECOM Cambodia Premuim Internet Service Provider">
 					<div class="EZECOM-logo">
 						<img class="img-responsive" src="<?php echo base_url()?>images/ezecom-logo.svg" alt="EZECOM Cambodia Premuim Internet Service Provider"/>
 					</div>
@@ -34,15 +34,15 @@
 			<div class="col-xl-6 col-lg-6 col-md-6 col-xs-6" style="padding-right:0">
 				<div class="text-right">
 					<div class="language">
-						<span><img src="<?php echo base_url()?>images/flags/kh.svg" id="2" onclick="change_language(this.id)" alt="Khmer" title="Khmer"></span>
-						<span><img src="<?php echo base_url()?>images/flags/en.svg" id="1" onclick="change_language(this.id)" alt="English" title="English"></span>
-						<span><img src="<?php echo base_url()?>images/flags/ch.svg" id="3" onclick="change_language(this.id)" alt="Chinese" title="Chinese"></span>
+						<span><img src="<?php //echo base_url()?>images/flags/kh.svg" id="2" onclick="change_language(this.id)" alt="Khmer" title="Khmer"></span>
+						<span><img src="<?php //echo base_url()?>images/flags/en.svg" id="1" onclick="change_language(this.id)" alt="English" title="English"></span>
+						<span><img src="<?php //echo base_url()?>images/flags/ch.svg" id="3" onclick="change_language(this.id)" alt="Chinese" title="Chinese"></span>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
+</div>-->
 
 <script>
 
@@ -50,10 +50,10 @@
 	window.onscroll = function() {scrollFunction()};
 	function scrollFunction() {
 		if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-			document.getElementById("top-header-fix").style.top = "0";
-			document.getElementById("menu-fix").style.top = "60px";
+			//document.getElementById("top-header-fix").style.top = "0";
+			document.getElementById("menu-fix").style.top = "0";
 		} else {
-			document.getElementById("top-header-fix").style.top = "-80px";
+			//document.getElementById("top-header-fix").style.top = "-80px";
 			document.getElementById("menu-fix").style.top = "-100px";
 		}
 	}
